d0b2c2e329cacf8c3ed049364a57127b04ee6bf4
Todd Powell <toddwpowell@gmail.com>
2023-09-13 16:35:33 -0600

    Add css for panel sizes. Add right content.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import "./App.css";
 import ProductPreviewCard from "./components/ProductPreviewCard";
+<style>
+  @import url('https://fonts.google.com/specimen/Montserrat'); @import
+  url('https://fonts.google.com/specimen/Fraunces');
+</style>;
 
 function App() {
   return (
--- src/components/ProductPreviewCard.tsx
+++ src/components/ProductPreviewCard.tsx
@@ -3,25 +3,41 @@ import "./ProductPreviewCard.css";
 const ProductPreviewCard = () => {
   return (
     <>
-      <div className="prc-wrapper">
-        <div className="prc-left-panel">
-          <picture>
-            <source
-              media="(min-width:650px)"
-              srcSet="./src/assets/images/image-product-desktop.jpg"
-            />
-            <source
-              media="(min-width:465px)"
-              srcSet="./src/assets/images/image-product-mobile.jpg"
-            />
-            <img
-              src="./src/assets/images/image-product-mobile.jpg"
-              alt="Flowers"
-            />
-          </picture>
-        </div>
+      <div className="app-container">
+        <div className="prc-wrapper">
+          <div className="prc-left-panel">
+            <>
+              <picture>
+                <source
+                  media="(min-width:650px)"
+                  srcSet="./src/assets/images/image-product-desktop.jpg"
+                />
+                <source
+                  media="(min-width:465px)"
+                  srcSet="./src/assets/images/image-product-mobile.jpg"
+                />
+                <img
+                  src="./src/assets/images/image-product-mobile.jpg"
+                  alt="Flowers"
+                />
+              </picture>
+            </>
+          </div>
 
-        <div className="prc-right-panel">content</div>
+          <div className="prc-right-panel">
+            <div>
+              <p>PERFUME</p>
+              <h1>Gabrielle Essence Eau De Parfum</h1>
+              <p>
+                A floral, solar and voluptuous interpretation composed by
+                Olivier Polge, Pervumer-Creator for the House of CHANEL.
+              </p>
+              <div className="sale-price">$149.99</div>
+              <div className="reg-price">$169.99</div>
+              <button>Add to Cart</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
